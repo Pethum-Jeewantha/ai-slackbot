0ba34e5850c144daf68237549af028e36bf4cd32
Pethum Jeewantha <pethumjeewantha1@gmail.com>
2023-04-19 21:23:50 +0530

    implement printCommandEvents

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/shomali11/slacker"
+	"log"
+	"os"
+)
 
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
+
+func main() {
+	err := godotenv.Load(".denv")
+	if err != nil {
+		log.Fatalln(".env file not found")
+	}
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
 }
