e005c1fc3284c2114c1b4f80ae1c3d256cde9118
Pethum Jeewantha <pethumjeewantha1@gmail.com>
2023-04-19 23:52:32 +0530

    add LICENSE.txt

--- main.go
+++ main.go
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2023 - present Pethum Jeewantha. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
 package main
 
 import (
